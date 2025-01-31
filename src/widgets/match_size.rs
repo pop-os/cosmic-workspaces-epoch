572c6ecc2e3ae191d07a963b42685253cb05a9da
Ian Douglas Scott <idscott@system76.com>
2025-01-31 14:17:56 -0800

    WIP Workspace pinning support
    WIP workspace drag
    
    Handle workspace drag
    
    Sort workspaces
    
    should be in cctk?
    
    Sort without clone
    
    Cargo.lock
    
    Fix drag of workspace to immediately following workspace
    
    fmt
    
    fix object version test
    
    WIP workspace drag placeholder
    
    WIP disable drag indicators
    
    WIP fix multiple drag targets overlapping with same id
    
    WIP style for hover over workspace
    
    Use visibility_wrapper
    
    Need way to make sure title is centered regardless
    
    Fix has_cursor being reset
    
    view: split pin button into a function
    
    correct colors for pin button
    
    WIP DropTarget::WorkspaceSidebarDragPlaceholder
    
    WIP drag and drop placeholder
    
    WIP remve worksapce while being dragged
    
    need a drag source?
    
    share some code for drag source
    
    Don't show placeholder when dragging toplevel to worksapce
    
    empty space widget as drag source when removing source
    
    still get "No source surface" error?
    
    remove commented code
    
    use visibility_wrapper instead of removing element
    
    may work betteer. fixes "no source surface" at least
    
    remove commented
    
    fix where visibility_wrapper is used
    
    Make placeholder use a placeholder widget, but match size of
    workspace_item
    
    better styling for drag placeholder than just black image
    
    Don't add placeholder for drag over same workspace
    
    Don't allow drag to next workspace
    
    Workaround for `SourceFinished` issue
    
    Use fixed widget ID for workspace dnd_soruce
    
    May help with some things. But doesn't seem to fix SourceFinished.
    
    Remove workspace from list when starting drag
    
    Doesn't always work?
    
    Need to not allow drag of empty last workspace.
    
    Ignore drag that shoudn't move workspace
    
    We never need to insert after a workspace with this design, since the
    empty last workspace shouldn't have a workspace added after it.
    
    Remove now unneeded use of `visibility_wrapper`
    
    Swap workspace dnd source/destination
    
    Seems to help with "No source surface" issues? Still in some cases? Drag
    cancellation is still an issue.
    
    Workaround for on_cancel issue
    
    Not sure how to fix state diffing here in libcosmic/iced. But this is
    robust.
    
    Don't allow dragging empty and unpinned workspace
    
    Don't add spacing for invisible widget
    
    Hide active borders while dragging workspace
    
    Matches design (other than lack of animation) and avoids visual
    confusion since it's the same as the placeholder where a workspace will
    be dropped. Seems good with one monitor, though with multiole, it's a
    little distracting seeing border disapear on othe routputs too.
    
    Center workspace titles
    
    a little off center due to pin button. avoiding that may require custom
    widget or funny tricks like an insible spacer matching the size of the
    button...

---
+++ src/widgets/match_size.rs
@@ -0,0 +1,145 @@
+//! Show one surface, sized to match the size of another (invisible) widget
+
+use cosmic::iced::{
+    advanced::{
+        layout, mouse, renderer,
+        widget::{Operation, Tree},
+        Clipboard, Layout, Shell, Widget,
+    },
+    event::{self, Event},
+    Length, Rectangle, Size,
+};
+use std::marker::PhantomData;
+
+pub fn match_size<
+    'a,
+    Msg,
+    T1: Into<cosmic::Element<'a, Msg>>,
+    T2: Into<cosmic::Element<'a, Msg>>,
+>(
+    matched: T1,
+    shown: T2,
+) -> MatchSize<'a, Msg> {
+    MatchSize {
+        matched: matched.into(),
+        shown: shown.into(),
+        _msg: PhantomData,
+    }
+}
+
+pub struct MatchSize<'a, Msg> {
+    matched: cosmic::Element<'a, Msg>,
+    shown: cosmic::Element<'a, Msg>,
+    _msg: PhantomData<Msg>,
+}
+
+impl<Msg> Widget<Msg, cosmic::Theme, cosmic::Renderer> for MatchSize<'_, Msg> {
+    delegate::delegate! {
+        to self.matched.as_widget() {
+            fn size(&self) -> Size<Length>;
+            fn size_hint(&self) -> Size<Length>;
+        }
+    }
+
+    fn operate(
+        &self,
+        tree: &mut Tree,
+        layout: Layout<'_>,
+        renderer: &cosmic::Renderer,
+        operation: &mut dyn Operation<()>,
+    ) {
+        self.matched
+            .as_widget()
+            .operate(&mut tree.children[0], layout, renderer, operation);
+        self.shown
+            .as_widget()
+            .operate(&mut tree.children[1], layout, renderer, operation);
+    }
+
+    fn on_event(
+        &mut self,
+        tree: &mut Tree,
+        event: Event,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        renderer: &cosmic::Renderer,
+        clipboard: &mut dyn Clipboard,
+        shell: &mut Shell<'_, Msg>,
+        viewport: &Rectangle,
+    ) -> event::Status {
+        self.shown.as_widget_mut().on_event(
+            &mut tree.children[1],
+            event,
+            layout,
+            cursor,
+            renderer,
+            clipboard,
+            shell,
+            viewport,
+        )
+    }
+
+    fn mouse_interaction(
+        &self,
+        tree: &Tree,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+        renderer: &cosmic::Renderer,
+    ) -> mouse::Interaction {
+        self.shown.as_widget().mouse_interaction(
+            &tree.children[1],
+            layout,
+            cursor,
+            viewport,
+            renderer,
+        )
+    }
+
+    fn layout(
+        &self,
+        tree: &mut Tree,
+        renderer: &cosmic::Renderer,
+        limits: &layout::Limits,
+    ) -> layout::Node {
+        // TODO?
+        self.matched
+            .as_widget()
+            .layout(&mut tree.children[0], renderer, limits)
+    }
+
+    fn draw(
+        &self,
+        tree: &Tree,
+        renderer: &mut cosmic::Renderer,
+        theme: &cosmic::Theme,
+        style: &renderer::Style,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+    ) {
+        self.shown.as_widget().draw(
+            &tree.children[1],
+            renderer,
+            theme,
+            style,
+            layout,
+            cursor,
+            viewport,
+        );
+    }
+
+    fn children(&self) -> Vec<Tree> {
+        vec![Tree::new(&self.matched), Tree::new(&self.shown)]
+    }
+
+    fn diff(&mut self, tree: &mut Tree) {
+        tree.diff_children(&mut [&mut self.matched, &mut self.shown]);
+    }
+}
+
+impl<'a, Msg: 'a> From<MatchSize<'a, Msg>> for cosmic::Element<'a, Msg> {
+    fn from(widget: MatchSize<'a, Msg>) -> Self {
+        cosmic::Element::new(widget)
+    }
+}
